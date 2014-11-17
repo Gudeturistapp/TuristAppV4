f23e3e4830207d31eade5cdf8b81d7d3075b4b0d
Danny <danny.nlsn0@gmail.com>
2014-11-17 14:29:13 +0100

    Revert "revert"
    This reverts commit 80c3a6bd13ca89192473f85ac2dedecc86de6548.

---
+++ TuristAppV4/TuristAppV4/Model/TestClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuristAppV4.Model
+{
+    class TestClass
+    {
+        //Just a test
+    }
+}
