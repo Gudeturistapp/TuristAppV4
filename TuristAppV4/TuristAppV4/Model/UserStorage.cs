cf4806354445280ad6f2a2c3e50b049e5d77e8cf
Danny <danny.nlsn0@gmail.com>
2014-11-17 16:12:38 +0100

    Roder rundt med LoginPage
    Minor problemz

---
+++ TuristAppV4/TuristAppV4/Model/UserStorage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuristAppV4.Model
+{
+    class UserStorage
+    {
+        public Dictionary<string, string> dictionary = new Dictionary<string, string>(); 
+    }
+}
---
+++ TuristAppV4/TuristAppV4/View/MainPageLogin.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+using TuristAppV4.Model;
+
+namespace TuristAppV4
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Dictionary<string,string> dictionary = new Dictionary<string, string>(); 
+
+        public MainPage()
+        {
+
+            this.InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (NameBox.Text != "" && PassBox.Password != "")
+            {
+                 
+                
+                dictionary.Add(PassBox.Password, NameBox.Text);
+
+                Frame.Navigate(typeof(View.MainPage));
+
+
+
+            }
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+           
+
+            if (dictionary.ContainsKey(PassBox.Password) &&
+                dictionary.ContainsValue(NameBox.Text))
+            {
+                Frame.Navigate(typeof (View.MainPage));
+            }
+            else
+            {
+                ErrorBox.Text = "Error! Couldn't find user";
+            }
+        }
+    }
+}
